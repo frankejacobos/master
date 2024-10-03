dc08b41649a9d3c67b5ef881b5385535d5b9e013
Frank Jacobo <frankejacobos@gmail.com>
2024-10-03 16:48:11 -0500

    :arrow_up: (azuiv2): update version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TipoRepuestaEnum } from 'azuiv2';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,18 @@ import { TipoRepuestaEnum } from 'azuiv2';
 })
 export class AppComponent {
   public readonly TipoAlerta = TipoRepuestaEnum;
+  public readonly afterUpload: (file: NzUploadFile) => boolean = (file) => {
+    console.log({ file });
+    return true;
+  };
+
+  isVisible: boolean = false;
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  closeModal(): void {
+    this.isVisible = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AzuiAlertModule, AzuiButton } from 'azuiv2';
+import {
+  AzuiAlertModule,
+  AzuiButton,
+  AzuiCheckboxComponent,
+  AzuiCopyComponent,
+  AzuiDividerComponent,
+  AzuiErrorComponent,
+  AzuiFileUploadModule,
+  AzuiIcons,
+  AzuiInputModule,
+  AzuiInputNumberComponent,
+  AzuiMessageComponent,
+  AzuiModalComponent,
+  AzuiTextareaComponent,
+} from 'azuiv2';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AzuiButton,
     AzuiAlertModule,
+    AzuiButton,
+    AzuiCheckboxComponent,
+    AzuiCopyComponent,
+    AzuiDividerComponent,
+    AzuiErrorComponent,
+    AzuiFileUploadModule,
+    AzuiIcons,
+    AzuiInputModule,
+    AzuiInputNumberComponent,
+    AzuiMessageComponent,
+    AzuiModalComponent,
+    AzuiTextareaComponent,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
